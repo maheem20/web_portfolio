740c0a47b096f78a661108dba0f6fe466834eaf8
maheem_20 <mwistanbuly@gmail.com>
2025-01-30 00:33:29 +0100

    feat: update Header component with sticky positioning and improved styling

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,19 +3,24 @@
 
     let tabs = [
         { name: "Projects", link: "#projects" },
-        { name: "About me", link: "#about" }
-    ]
+        { name: "About me", link: "#about" },
+    ];
 </script>
 
-<header>
-    <h1>
-        <b>Maheem</b>
+<header
+    class={"sticky z-10 top-0 duration-200 px-6 flex items-center justify-between border border-solid " +
+        (y > 0
+            ? " py-4 bg-slate-950 border-violet-950"
+            : " py-6 bg-transparent border-transparent")}
+>
+    <h1 class="font-medium">
+        <b class="font-bold poppins">Maheem</b>
     </h1>
-    <div class="">
-        {#each tabs as tab, index }
-            <a>
+    <div class="sm:flex items-center gap-4 hidden">
+        {#each tabs as tab, index}
+            <a href={tab.link} class="duration-200 hover:text-violet-400">
                 <p>{tab.name}</p>
-            </a>            
+            </a>
         {/each}
     </div>
 </header>
