455f8f879ab5fd4ab53592b510a70479deaa4f0e
maheem_20 <mwistanbuly@gmail.com>
2025-01-30 00:16:37 +0100

    feat: add Header component with navigation tabs for Projects and About me

---
+++ src/components/Header.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let y;
+
+    let tabs = [
+        { name: "Projects", link: "#projects" },
+        { name: "About me", link: "#about" }
+    ]
+</script>
+
+<header>
+    <h1>
+        <b>Maheem</b>
+    </h1>
+    <div class="">
+        {#each tabs as tab, index }
+            <a>
+                <p>{tab.name}</p>
+            </a>            
+        {/each}
+    </div>
+</header>
