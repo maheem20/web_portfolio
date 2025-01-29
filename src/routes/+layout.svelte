458089c266aea55411b1eaeb7cee4df6e6aa4b8c
maheem_20 <mwistanbuly@gmail.com>
2025-01-30 00:16:26 +0100

    feat: enhance scroll-to-top button with click functionality and styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,11 +15,19 @@
 <div
 	class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen"
 >
-	<div class={"fixed bottom-0 w-full duration-200 flex p-10 z-10 " + (
-		y > 0 ? ' opacity-full pointer-events-auto': ' pointer-events-none opacity-0' 
-	)}>
-		<button>
-			<i class="fa-solid fa-arrow-up" />
+	<div
+		class={"fixed bottom-0 w-full duration-200 flex p-10 z-10 " +
+			(y > 0
+				? " opacity-full pointer-events-auto"
+				: " pointer-events-none opacity-0")}
+	>
+		<button
+			on:click={goTop}
+			class="ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer"
+		>
+			<i
+				class="fa-solid fa-arrow-up grid place-items-center aspect-square"
+			/>
 		</button>
 	</div>
 	<Header />
