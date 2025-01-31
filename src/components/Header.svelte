914a5df77a4ea4ffed811490b7be9d550390e036
maheem_20 <mwistanbuly@gmail.com>
2025-01-31 16:08:35 +0100

    feat: improve layout and styling of Header component

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -16,17 +16,21 @@
     <h1 class="font-medium">
         <b class="font-bold poppins">Maheem</b>
     </h1>
-    <div class="sm:flex items-center gap-4 hidden">
+    <div class="sm:flex ml-auto pr-4 items-center gap-4 hidden">
         {#each tabs as tab, index}
             <a href={tab.link} class="duration-200 hover:text-violet-400">
                 <p>{tab.name}</p>
             </a>
         {/each}
     </div>
-    <a href="linkedin.com" target="_blank"
+    <a
+        href="linkedin.com"
+        target="_blank"
         class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
     >
-        <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"></div>
+        <div
+            class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+        ></div>
         <h4 class="relative z-9">Get in touch</h4>
     </a>
 </header>
