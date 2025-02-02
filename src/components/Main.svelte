b2ea40c461d2c7a35d15dd72b10f08e7f341e152
maheem_20 <mwistanbuly@gmail.com>
2025-02-02 21:10:11 +0100

    fix: adjust button alignment

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -45,7 +45,7 @@
                 or Firebase/Firestore!
             </p>
             <a
-                class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer"
+                class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer"
                 href="linkedin.com"
                 target="_blank"
             >
