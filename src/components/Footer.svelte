93a14e1b34cfbe93e9e3400538202934895ea9c7
maheem_20 <mwistanbuly@gmail.com>
2025-02-02 21:10:00 +0100

    feat: add contact information to footer component

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -4,4 +4,31 @@
     <p class="px-4 py-2 bg-white text-slate-950 font-medium">
         Connect with me &darr;
     </p>
+    <div class="flex flex-col gap-4 items-center justify-center">
+        <p><b class="pr-2">Email</b> maheem_pome@gmail.com</p>
+        <p>
+            <b class="pr-2">Github</b>
+            <a class="text-violet-400" target="_blank" href="#">
+                maheem_pome<sup
+                    ><span
+                        ><i
+                            class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+                        ></i>
+                    </span>
+                </sup>
+            </a>
+        </p>
+        <p>
+            <b class="pr-2">LinkedIn</b>
+            <a class="text-violet-400" target="_blank" href="#">
+                maheem_pome<sup
+                    ><span
+                        ><i
+                            class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+                        ></i>
+                    </span>
+                </sup>
+            </a>
+        </p>
+    </div>
 </footer>
